bf4dcc8b98f666961aead5b8b2e84534b6f24aad
AndrMe <124451118+AndrMe@users.noreply.github.com>
2023-10-07 15:26:11 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,11 +1,12 @@
 '''core file makes the programm work'''
 import pygame
 import simulation
+import renderer
 
 
 def init():
     ''' load all the configs,init all the objects'''
-    #pseudo: load_data() -> planets, delta_time, trace,v_dict, (init_dict)
+    #load_data() -> planets, delta_time, trace,v_dict, (init_dict)
     # StartPygame() like surface=pygame.surface.Surface((height,width))
     pygame.init()
     delta_time=1
@@ -15,25 +16,28 @@ def init():
     g=1
     scr_height=1000
     scr_width=1200
-    sim_core=simulation.Simulation(planets,trace,g,delta_time)
-    surface=pygame.surface.Surface((scr_height,scr_width))
-    return surface,sim_core
+    sim_core=simulation.Simulation(planets,g,delta_time)
+    screen = pygame.display.set_mode((scr_width, scr_height),pygame.RESIZABLE)
+    render=renderer.Renderer(screen,scr_height,scr_width,scale,trace) #trace is renderer's
+    return render,sim_core
 
-def run_program(renderer,sim_core):
+def run_program(render=renderer.Renderer,sim_core=simulation.Simulation):
     '''Run the loop'''
     running=True
     stopped=False
     while running:
         if not stopped:
             sim_core.sim_cycle()
-        renderer.new_frame()
+            render.transit_trace(sim_core.get_trace_updates())
+            render.new_frame()
+        running=False #debug
         
 def end_program():
     '''all that should be done when shutting down the program'''
-    pass
+    print('ending')
 
 if __name__=='__main__':
     '''All works from here'''
-    renderer,sim_core=init()
-    run_program(renderer,sim_core)
+    render,sim_core=init()
+    run_program(render,sim_core)
     end_program()
