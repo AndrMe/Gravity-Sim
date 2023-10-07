7f7d48264efcbc9b7d6c4521a9698f63e00b5c64
AndrMe <124451118+AndrMe@users.noreply.github.com>
2023-10-07 15:27:10 +0300

    Create renderer.py

---
+++ renderer.py
@@ -0,0 +1,14 @@
+'''makes frames to display'''
+class Renderer():
+    def __init__(self,screen,height,width,scale,trace):
+        self.screen=screen
+        self.height=height
+        self.width=width
+        self.scale=scale
+        self.trace=trace
+    def transit_trace(self,trace):
+        '''get list of tace cords -> list of separate lines'''
+        pass
+    def new_frame(self):
+        '''Draws new frame'''
+        pass
