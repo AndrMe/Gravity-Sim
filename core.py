5479759d934313d09b90d62fba02a28645b45b7c
AndrMe <124451118+AndrMe@users.noreply.github.com>
2023-10-20 22:26:58 +0300

    Update core.py (working blackscreen)

--- core.py
+++ core.py
@@ -2,11 +2,10 @@
 import pygame
 import simulation
 import renderer
-
-
+import controls
 def init():
     ''' load all the configs,init all the objects'''
-    #load_data() -> planets, delta_time, trace,v_dict, (init_dict)
+    #load_data() -> planets, delta_time, trace,v_dict, (configs)
     # StartPygame() like surface=pygame.surface.Surface((height,width))
     pygame.init()
     delta_time=1
@@ -19,18 +18,27 @@ def init():
     sim_core=simulation.Simulation(planets,g,delta_time)
     screen = pygame.display.set_mode((scr_width, scr_height),pygame.RESIZABLE)
     render=renderer.Renderer(screen,scr_height,scr_width,scale,trace) #trace is renderer's
-    return render,sim_core
-
-def run_program(render=renderer.Renderer,sim_core=simulation.Simulation):
+    configs={
+        'running':True,'stopped':True,'scale_power':1,'scr_height':scr_height,'scr_width':scr_width,
+        'g':g,'delta_time':delta_time
+        }
+    # interf=interface.Interface() -> renderer
+    return render,sim_core,configs
+def update_state(sim_core=simulation.Simulation,render=renderer.Renderer,configs=set()):
+    sim_core.update_state(configs)
+def run_program(render=renderer.Renderer,sim_core=simulation.Simulation,configs={}):
     '''Run the loop'''
-    running=True
-    stopped=False
-    while running:
-        if not stopped:
+    configs['running']=True
+    configs['stopped']=True
+    while configs['running']:
+        configs=controls.update(pygame.event.get(),sim_core,render,configs)
+
+        render.new_frame()
+        if not configs['stopped']: 
             sim_core.sim_cycle()
             render.transit_trace(sim_core.get_trace_updates())
-            render.new_frame()
-        running=False #debug
+            
+        #configs['running']=False #debug
         
 def end_program():
     '''all that should be done when shutting down the program'''
@@ -38,6 +46,6 @@ def end_program():
 
 if __name__=='__main__':
     '''All works from here'''
-    render,sim_core=init()
-    run_program(render,sim_core)
+    render,sim_core,configs=init()
+    run_program(render,sim_core,configs)
     end_program()
